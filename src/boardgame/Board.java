19fc82334cde8206b208d71339a8d04b55fae493
beatriz <biafagundes09@gmail.com>
2024-08-14 16:01:56 -0300

    Movendo as peças

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -41,13 +41,24 @@ public class Board {
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
     }
+    public Piece removePiece(Position position){
+        if (!positionExists(position)){
+            throw new BoardException("Não há essa posição no tabuleiro.");
+        }if(piece(position) == null){
+            return null;
+        }
+        Piece aux = piece(position);
+        aux.position = null;
+        pieces[position.getRow()][position.getColumn()] = null;
+        return aux;
+    }
     private boolean positionExists(int row, int column){
        return row >= 0 && row < rows && column >= 0 && column < columns;
     }
     private boolean positionExists(Position position){
         return positionExists(position.getRow(), position.getColumn());
     }
-    private boolean thereIsAPiece (Position position){
+    public boolean thereIsAPiece (Position position){
         if(!positionExists(position)){
             throw new BoardException("A posição não existe no tabuleiro.");
         }
