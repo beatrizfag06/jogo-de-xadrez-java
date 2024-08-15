0c65e3e081ae6c3b2641735825cb47948ea745c4
beatriz <biafagundes09@gmail.com>
2024-08-15 10:34:09 -0300

    Implementando possíveis movimentos da torre.

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -9,10 +9,6 @@ public class Position {
         this.column = column;
     }
 
-    public void setValues(int row, int column){
-
-    }
-
     public int getRow() {
         return row;
     }
@@ -29,6 +25,10 @@ public class Position {
         this.column = column;
     }
 
+    public void setValues(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
     @Override
     public String toString() {
         return row + ", " + column;
