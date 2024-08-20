ca36a40ec6427161a84eafddf5bd30dafe148bec
beatriz <biafagundes09@gmail.com>
2024-08-20 14:59:56 -0300

    Correção: Melhor tratamento para promoção

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,8 +36,12 @@ public class Program {
                 }
 
                 if(chessMatch.getPromoted() != null){
-                    System.out.println("Entre com uma peça para promoção: (B/N/R/Q)");
+                    System.out.print("Entre com uma peça para promoção: (B/N/R/Q): ");
                     String type = sc.nextLine().toUpperCase();
+                    while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")){
+                        System.out.print("Valor inválido! Entre com uma peça para promoção: (B/N/R/Q): ");
+                        type = sc.nextLine().toUpperCase();
+                    }
                     chessMatch.replacePromotedPiece(type);
                 }
            } catch (ChessException e) {
